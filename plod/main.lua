45ca7d3b194b559c526b84d9631c88c930f743b3
Mason Staugler <mason@staugler.net>
2011-02-07 22:52:08 -0500

    plod: Added mission mode and new argument parser.
    Mission mode listens for mission updates and tries to determine a
    destination sector and trade item. If it succeeds, it will execute
    normal plod behavior with those values.
    
    I also changed the argument parser for the manual definitions because I
    got annoyed typing in quotes.

--- plod/main.lua
+++ plod/main.lua
@@ -4,21 +4,32 @@
 -- Author: INKling
 --
 -- Plod can be used to plot courses between two sectors. It will also buy and
--- unload cargo between two stations, if you provide a third argument. For
--- trading it will do everything but the flying.
+-- unload cargo between two stations, if you provide it after the second sector
+-- definition. For trading it will do everything but the flying.
 --
 -- The trade item is case sensitive.
 --
+-- Plod also has a mission mode. When active, it will listen for mission
+-- updates and try to find an destination sector and an item to trade. This has
+-- been tested mainly against hive skirmish and trading guild missions, though
+-- it should work with any mission I haven't thought of.
+--
 -- Usage.
 -- ------
--- /plod [start sector or 'current'] [end sector or 'current'] [optional trade item]
+-- /plod [from sector] [to sector] [optional trade item]
+--
+-- /plod mission
+--     Turns on mission mode.
+--
+-- /plod off
+--     Turns plod off.
 --
 -- Examples:
 -- ---------
--- /plod 'sol e15' 'geira i5' 'XiRite Alloy'
--- /plod current 'geira o4' 'Basic Targeting Systems'
+-- /plod sol e15 geira i5 XiRite Alloy
+-- /plod . geira o4 Basic Targeting Systems
 -- /plod a12 j6
-plod = {}
+plod = plod or {}
 
 plod.on = false
 plod.to = nil
@@ -30,90 +41,179 @@ plod.timing.timer = Timer()
 plod.timing.timer_interval = 1000
 plod.timing.on = false
 
+plod.mission_watch = false
+plod.mission_last_message = nil
+
 function plod.pinfo(str)
-    print("\127c4a000plod: "..str.."\127o")
+    print("\127c4a000plod: "..tostring(str).."\127o")
 end
 
 function plod.perr(str)
-    print("\127a40000plod: "..str.."\127o")
+    print("\127a40000plod: "..tostring(str).."\127o")
 end
 
 function plod.ppass(str)
-    print("\1274e9a06plod: "..str.."\127o")
+    print("\1274e9a06plod: "..tostring(str).."\127o")
 end
 
 -- ============
--- Plod: start.
+-- Plod: usage.
 -- ============
--- Starts plodding between two sectors.
-function plod.cmd_plod(data, args)
-    -- Unregister events to be sure that multiple calls don't increase the number of
-    -- listeners.
-    UnregisterEvent(plod.plodding_sector_changed, "SECTOR_CHANGED")
-    UnregisterEvent(plod.plodding_trade, "ENTERED_STATION")
-
-    -- No input.
-    if not args then
+function plod.usage()
         plod.pinfo("NAME")
         plod.pinfo("        plod - For plodding between two sectors and optionally moving cargo.")
         plod.pinfo("SYNOPSIS")
         plod.pinfo("        /plod [start sector] [end sector] [optional trade item]")
         plod.pinfo("EXAMPLES")
-        plod.pinfo("        /plod 'sol e15' 'geira i5' 'XiRite Alloy'")
-        plod.pinfo("        /plod current 'geira o4' 'Basic Targeting Systems'")
+        plod.pinfo("        /plod sol e15 geira i5 XiRite Alloy")
+        plod.pinfo("        /plod . geira o4 Basic Targeting Systems")
         plod.pinfo("        /plod a12 j6")
-        return
+end
+
+-- ============
+-- Plod: reset.
+-- ============
+-- Resets the application state.
+function plod.reset()
+    plod.on = false
+    plod.to = nil
+    plod.from = nil
+    plod.trade_item = nil
+    plod.mission_watch = false
+
+    -- Unregister events to be sure that multiple calls don't increase the number of
+    -- listeners.
+    UnregisterEvent(plod.plodding_sector_changed, "SECTOR_CHANGED")
+    UnregisterEvent(plod.plodding_trade, "ENTERED_STATION")
+end
+
+-- ====================
+-- Plod: get sector ID.
+-- ====================
+-- args
+--     A list of arguments to parse. This function is destructive and removes
+--     leading elements as it parses.
+-- return
+--     A sector ID or nil.
+function plod.get_sector_id(args)
+    if not args then
+        return nil
+    end
+
+    local sector_id = nil
+    local system_name = nil
+
+    while sector_id == nil and # args > 0 do
+        local arg = args[1]
+
+        -- Current sector.
+        if arg == '.' or arg == 'current' then
+            sector_id = GetCurrentSectorid()
+
+        -- Sector definition.
+        elseif arg:match("%a%-?%d") then
+            if not system_name then
+                system_name = string.gsub(SystemNames[GetCurrentSystemid()], '(%S+)%s*(%S*)', '%1')
+            end
+            sector_id = SectorIDFromLocationStr(system_name..' '..arg)
+
+        -- System definition.
+        elseif arg:match("%w+") then
+            if not system_name then
+                system_name = arg
+            else
+                -- We already found a system name and got another. System names
+                -- are one word.
+                return nil
+            end
+
+        -- Final error condition.
+        else
+            return nil
+
+        end
+
+        table.remove(args, 1)
+    end
+
+    return sector_id
+end
+
+-- =================
+-- Plod: user input.
+-- =================
+-- Starts plodding between two sectors.
+function plod.cmd_plod(data, args)
+    plod.reset()
+
+    -- No input.
+    if not args then
+        plod.usage()
     end
 
     -- Turn it off.
     if args[1] and args[1] == 'off' then
-        plod.on = false
+        -- We should have already reset, so just tell the user.
         plod.ppass("We're no longer tracking you.")
         return
     end
 
-    if not args[1] or not args[2] then
-        plod.perr("You must provide two sector names.")
+    -- Listen for a mission.
+    if args[1] and args[1] == 'mission' then
+        plod.on = true
+        plod.mission_watch = true
+        plod.pinfo('Trying last mission update.')
+        plod.mission_update(plod.mission_last_message)
         return
     end
 
-    -- Plot course.
-    local from = nil
-    local current_system_name = string.gsub(SystemNames[GetCurrentSystemid()], '(%S+)%s*(%S*)', '%1')
-    if (args[1] == 'current' or args[1] == '.') then
-        from = GetCurrentSectorid()
-    elseif string.find(args[1], '^%a%--%d+') then
-        from = SectorIDFromLocationStr(current_system_name..' '..args[1])
-    else
-        from = SectorIDFromLocationStr(args[1])
+    -- Manually defined course.
+    plod.on = true
+    local from = plod.get_sector_id(args)
+    local to = plod.get_sector_id(args)
+    local trade_item = nil
+
+    if not from or not to then
+        plod.usage()
+        plod.reset()
+        plod.perr('We weren\'t able to determine the start and end sectors.')
+        return
     end
 
-    local to = nil
-    if (args[2] == 'current') then
-        to = GetCurrentSectorid()
-    elseif string.find(args[2], '^%a%--%d+') then
-        to = SectorIDFromLocationStr(current_system_name..' '..args[2])
-    else
-        to = SectorIDFromLocationStr(args[2])
+    -- If there are any arguments left, it's the trade item.
+    if # args > 0 then
+        trade_item = table.concat(args, ' ')
     end
 
-    plod.pinfo("Plodding you between " .. LocationStr(from) .. " and " .. LocationStr(to) .. ".")
-    NavRoute.SetFinalDestination(to)
+    plod.start(from, to, trade_item)
+end
 
+-- ============
+-- Plod: start.
+-- ============
+-- This will set the application state and register the events.
+--
+-- from
+--     The sectorid of the start sector.
+-- to
+--     The sectorid of the end sector.
+-- item
+--     Optional. The item to trade.
+function plod.start(from, to, item)
     plod.from = from
     plod.to = to
-    plod.on = true
+    plod.trade_item = item
 
-    RegisterEvent(plod.plodding_sector_changed, "SECTOR_CHANGED")
+    plod.pinfo("Plodding you between " .. LocationStr(from) .. " and " .. LocationStr(to) .. ".")
 
-    -- If they've provided a third argument, this is the item they're trading.
-    if args[3] then
-        local trade_item = args[3]
-        plod.trade_item = trade_item
-        plod.pinfo("We'll also move "..plod.trade_item.." for you.")
+    if item then
+        plod.pinfo("We'll also move "..item.." for you.")
 
         RegisterEvent(plod.plodding_trade, "ENTERED_STATION")
     end
+    RegisterEvent(plod.plodding_sector_changed, "SECTOR_CHANGED")
+
+    NavRoute.SetFinalDestination(to)
 end
 
 -- ====================
@@ -179,7 +279,7 @@ function plod.plodding_trade(event)
             end
         end
 
-        plod.perr("We weren't able to unload "..item..". This station doesn't sell it either. You probably made a mistake.")
+        plod.perr("We weren't able to unload "..item..". This station doesn't sell it either.")
     else
         -- Buy item.
         -- ---------
@@ -198,4 +298,82 @@ function plod.plodding_trade(event)
     end
 end
 
+-- ====================
+-- Plod: mission event.
+-- ====================
+-- Callback for the CHAT_MSG_SECTORD_MISSION and CHAT_MSG_MISSION events to
+-- store the last mission message and, if enabled, execute the plod mission
+-- behavior.
+--
+-- event
+--     The name of the event.
+--
+-- data
+--     A table: { missionid = ..., msg = ... }
+function plod.mission_event(event, data)
+    if not data then
+        return
+    end
+
+    local msg = data.msg
+
+    -- If plod is turned on after accepting a mission we want the last
+    -- message, so we'll always store the last message.
+    plod.mission_last_message = msg
+
+    if plod.on and plod.mission_watch then
+        plod.mission_update(msg)
+    end
+end
+
+-- =====================
+-- Plod: mission update.
+-- =====================
+-- message
+--     The mission text to parse.
+function plod.mission_update(message)
+    if not message then
+        plod.pinfo('We\'re waiting for a mission update.')
+        return
+    end
+
+    local items = {}
+    local sectors = {}
+    local cargo_pat = '(%d+) (%w+ ?%w* ?%w*) crates'
+    local sector_pat = '(%u%w+) ?(%u?%w*) (%u%-%d+)'
+
+    for number, item in message:gmatch(cargo_pat) do
+        table.insert(items, item)
+    end
+    for system_name1, system_name2, sector in message:gmatch(sector_pat) do
+        table.insert(sectors, system_name1..' '..sector)
+    end
+
+    -- We only want to start plod if there's one sector. If there are more,
+    -- they probably haven't selected a mission yet and we got updates from
+    -- mission menus.
+    if # sectors == 1 then
+        -- Always start from the current sector for missions.
+        local from = GetCurrentSectorid()
+        local to = SectorIDFromLocationStr(sectors[1])
+        local item = nil
+        if items and # items == 1 then
+            item = items[1]
+        end
+
+        if to == nil or from == nil then
+            plod.perr('There was an error looking up the sector ID.')
+        end
+
+        -- If we've calculated a sector we're already plodding between, then
+        -- we'll ignore the update. Otherwise docking at a station during a
+        -- mission will cause us to restart the plod.
+        if to ~= plod.to and to ~= plod.from then
+            plod.start(from, to, item)
+        end
+    end
+end
+
 RegisterUserCommand('plod', plod.cmd_plod)
+RegisterEvent(plod.mission_event, 'CHAT_MSG_SECTORD_MISSION')
+RegisterEvent(plod.mission_event, 'CHAT_MSG_MISSION')
