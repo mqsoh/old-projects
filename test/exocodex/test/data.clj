dffe7b79fe513bf802835c4bc9318f655f21dbea
Mason Staugler <mason@staugler.net>
2012-08-23 00:11:43 -0400

    Reworked the schema.
    After reading the following, I decided to make flatter namespaces. I have a
    strange feeling that it's not good, but I don't want to get trapped into making
    the schema perfect. I can always change it later.
    
    http://stackoverflow.com/questions/10357778/data-modeling-in-datomic

--- test/exocodex/test/data.clj
+++ test/exocodex/test/data.clj
@@ -16,10 +16,9 @@
     (let [earth (ffirst (data/query uri
                     '[:find ?name
                       :where
-                          [?planet :exocodex/name ?name]
-                          [?planet :exocodex/type :exocodex.type/planet]
-                          [?planet :exocodex.stellar/name "Sol"]
-                          [?planet :exocodex.planet/letter :exocodex.planet.letter/a]]))]
+                          [?planet :name ?name]
+                          [?planet :name/stellar "Sol"]
+                          [?planet :name/letter "a"]]))]
 
         (is (= "Earth" earth))))
 
@@ -28,18 +27,18 @@
     (let [sun (ffirst (data/query uri
                         '[:find ?name
                           :where
-                              [?star :exocodex/name ?name]
-                              [?star :exocodex/type :exocodex.type/star]
-                              [?star :exocodex.stellar/name "Sol"]]))]
+                              [?star :name ?name]
+                              [?star :loc/distance _]
+                              [?star :name/stellar "Sol"]]))]
 
-        (is (= "Sun" sun))))
+        (is (= "Sol" sun))))
 
 
 (deftest lazy-retrieval
     (let [entities (data/query-entities uri
-                        '[:find ?thing :where [?thing :exocodex/name "Earth"]])
+                        '[:find ?thing :where [?thing :name "Earth"]])
           earth (first entities)]
 
         (is (= (type entities) clojure.lang.LazySeq))
-        (is (= (:exocodex.stellar/name earth) "Sol"))
-        (is (= (:exocodex.planet/letter earth) :exocodex.planet.letter/a))))
+        (is (= (:name/stellar earth) "Sol"))
+        (is (= (:name/letter earth) "a"))))
