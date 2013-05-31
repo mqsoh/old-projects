321b74cccb135445ae931f69830187ca6b452df6
Mason Staugler <mason@staugler.net>
2013-05-31 14:47:36 +0000

    Added a warning if the comparison fails.

--- django_s3_etag_collectstatic/management/commands/collectstatic.py
+++ django_s3_etag_collectstatic/management/commands/collectstatic.py
@@ -21,9 +21,20 @@ def md5_for_file(f, block_size=2**20):
 class Command(collectstatic.Command):
     def delete_file(self, path, prefixed_path, source_storage):
         if self.storage.exists(prefixed_path):
-            remote_checksum = self.storage._wrapped.entries.get(prefixed_path).etag.strip('"')
-            local_checksum = md5_for_file(source_storage.open(path))
-            if remote_checksum != local_checksum:
-                return True
+            try:
+                remote_checksum = self.storage._wrapped.entries.get(prefixed_path).etag.strip('"')
+                local_checksum = md5_for_file(source_storage.open(path))
+
+            except Exception e:
+                print((
+                    'django-s3-etag-collecstatic error: Skipping etag/md5 '
+                    'comparison due to an exception ({}).'
+                ).format(e))
+
+            else:
+                if remote_checksum == local_checksum:
+                    return False
+                else:
+                    return True
 
         return super(Command, self).delete_file(path, prefixed_path, source_storage)
