73d48ee50a090f0af1708348115850891a67d34a
Mason Staugler <mason@staugler.net>
2013-09-08 21:37:24 +0000

    Added server lifetime.
    This is the second big thing I wanted out of this project. I would often
    accidentally leave a SimpleHTTPServer running and wouldn't notice until I tried
    to start it again.
    
    This commit implements a lifetime for the server that defaults to five minutes.

--- truss/truss.py
+++ truss/truss.py
@@ -9,6 +9,9 @@ Usage:
 """
 
 import argparse
+import datetime
+import multiprocessing
+import re
 import sys
 
 import core
@@ -16,8 +19,23 @@ import core
 
 def main():
     config = parse_args(sys.argv[1:])
-    print('Serving on port {}...'.format(config['port']))
-    core.make_server(**config).serve_forever()
+    lifetime = config.pop('lifetime')
+
+    server = multiprocessing.Process(target=core.make_server(**config).serve_forever)
+    server.start()
+
+    print((
+        'Serving directory {docroot} at {hostname}:{port} for {lifetime} '
+        'seconds with credentials {username}:{password}.'
+    ).format(lifetime=lifetime, **config))
+
+    start_time = datetime.datetime.now()
+    delta = datetime.timedelta(seconds=lifetime)
+    while True:
+        now = datetime.datetime.now()
+        if now - start_time > delta:
+            server.terminate()
+            sys.exit('Time expired; shutting down.')
 
 
 def parse_args(args):
@@ -30,6 +48,24 @@ def parse_args(args):
         A dictionary with the following keys: username, password, host, port,
         and docroot.
     """
+
+    def lifetime_type(input):
+        """Converts a human readable time input to seconds. The input is an
+        integer with an optional trailing multiple: 'h', 'm', or 'd'."""
+
+        multiple = 1
+        last_char = input[-1:]
+        if last_char == 'h':
+            multiple = 60 * 60
+        elif last_char == 'm':
+            multiple = 60
+        elif last_char == 'd':
+            multiple = 24 * 60 * 60
+
+        integer = int(re.sub('[^0-9]+', '', input))
+
+        return integer * multiple
+
     parser = argparse.ArgumentParser(description=sys.modules[__name__].__doc__,
                                      formatter_class=argparse.RawTextHelpFormatter)
 
@@ -37,6 +73,11 @@ def parse_args(args):
         'When creating the server, you can specify a host. I never use this.'))
     parser.add_argument('-d', '--docroot', default='.', help=(
         'The path to serve; defaults to the current directory.'))
+    parser.add_argument('-l', '--lifetime', default='5m', type=lifetime_type, help=(
+        'The length of time to leave the server running; defaults to "5m". '
+        'Examples of valid values are: 300 (300 seconds), 5m (five minutes), 3h '
+        '(three hours), and 1d (one day).'
+    ))
     parser.add_argument('username', help='The accepted user name for basic HTTP auth.')
     parser.add_argument('password', help='The accepted password for basic HTTP auth.')
     parser.add_argument('port', type=int, help='The port to which we are binding.')
